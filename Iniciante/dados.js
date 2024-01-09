e7b9a88598de17ed6e1ab0ed5a9c33c5b87bca84
cahetterich <hetterichcarla@gmail.com>
2024-01-09 17:10:39 +0100

    origamid js

--- Iniciante/dados.js
+++ Iniciante/dados.js
@@ -109,3 +109,82 @@ console.log(typeof meuNome);
 
 var verificarTipoNome = typeof meuNome;
 console.log(verificarTipoNome);
+
+/*
+NÚMEROS E OPERADORES ARITMÉTICOS
+*/
+
+//numeros
+var idade1 = 28;
+var gols1 = 1000;
+var pi = 3.14; // ponto para decimal
+var exp = 2e10; // 20000000000 = 2 e 10 zeros
+
+console.log(idade1); // quando é um number aparece em azul no console
+
+//operadores
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 2*2*2*2 = 16
+var modulo = 14 % 5; // 4 = resto inteiro  *10 dividico por 5 é 2, mas sobra o 4 que é nosso resto inteiro da divisão
+
+
+/*
+OPERADORES ARITMÉTICOS (STRINGS)
+*/
+
+//Lembrando que soma + em Strings serve para concatenar e não somar
+var soma1 = '100' + 50; // 10050  - é uma string
+// na subtração, multiplicação e divisão, se houver uma string apenas com números o JS fará a conta
+var subtracao1 = '100' - 50; // 50   
+var multiplicacao1 = '100' * '2'; // 200
+// porém, se ouver uma frase escrita, aparecerá como NaN =
+var divisaoNan = 'Comprei 10' / 2; // NaN (Not a Number)
+// É possível verificar se uma variável é NaN ou não com a função isNaN()
+
+console.log(divisaoNan);
+//ou
+console.log(isNaN(divisaoNan)); // aparecerá true
+
+/* 
+NAN = NOT A NUMBER
+ */
+
+var numero = 80;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg' - string
+var pesoPorDois = peso / 2 // NaN (Not a Number)
+console.log(pesoPorDois);
+
+/* 
+A ORDEM IMPORTA
+
+Começa por multiplicação e divisão, depois por soma e subtração.
+ */
+
+var total1 = 20 + 5 * 2; // 30
+//Parênteses para priorizar uma expressão
+var total2 = (20 + 5) * 2; // 50 
+var total3 = 20 / 2 * 5; // 50
+var total4 = 10 + 10 + 20 + 30 * 4 / 2 + 10; // 40
+//primeiro multiplicação '30 * 4' = 120
+//segundo divisão por '/ 2' = 60
+//terceiro continua a ordem normal de soma = 110
+console.log(total4);
+
+
+/* 
+OPERADORES ARITMÉTICOS UNÁRIOS 
+*/
+
+var incremento = 5;
+console.log(incremento++); // 5
+console.log(incremento); // 6
+
+var incremento2 = 5;
+console.log(++incremento2); // 6
+console.log(incremento2); // 6
+
+//Mesma coisa para o decremento --x
