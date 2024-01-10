0ff344551ec103430c48dd8324dcac5f445cbd2c
cahetterich <hetterichcarla@gmail.com>
2024-01-10 13:59:37 +0100

    origamid js

--- Iniciante/booleanCondicionais.js
+++ Iniciante/booleanCondicionais.js
@@ -15,3 +15,17 @@ CONDICIONAIS IF E ELSE
 
 Verificar se uma expressão é verdadeira com if, caso contrário o else será ativado.
 */
+
+var possuiGraduacao = true;
+
+if(possuiGraduacao) {
+  console.log('Possui graduação');
+} else {
+  console.log('Não possui graduação');
+}
+// retorna Possui Graduação e não executa o else
+
+
+
+
+//valor dentro dos parênteses sempre será avaliado em false ou true.
