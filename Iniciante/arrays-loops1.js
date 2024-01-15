328a57f77342fa8476908fac7aec6837f0ecfd5e
cahetterich <hetterichcarla@gmail.com>
2024-01-15 22:07:32 +0100

    origamid js

---
+++ Iniciante/arrays-loops1.js
@@ -0,0 +1,20 @@
+/**
+ Arrays e Loops
+ */
+
+ /*
+ ARRAY
+
+ É um grupo de valores geralmente relacionados. Servem para guardarmos diferentes valores em uma única variável
+ */
+
+ var videoGames = ['Switch', 'PS4', 'XBox'];
+
+videoGames[0] // Switch
+videoGames[2] // Xbox
+
+//Acesse um elemento da array utilizando [n]
+
+/*
+MÉTODOS E PROPRIEDADES DE UMA ARRAY
+*/
