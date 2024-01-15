7986fa3bbefd1d969b2562fb9271e968e68fefc0
cahetterich <hetterichcarla@gmail.com>
2024-01-15 22:15:33 +0100

    origamid js

--- Iniciante/arrays-loops1.js
+++ Iniciante/arrays-loops1.js
@@ -11,7 +11,7 @@
  var videoGames = ['Switch', 'PS4', 'XBox'];
 
 videoGames[0] // Switch
-videoGames[2] // Xbox
+console.log(videoGames[2]); // Xbox
 
 //Acesse um elemento da array utilizando [n]
 
