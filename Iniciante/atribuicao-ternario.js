577804f9b8deaadb82fe9dca83dbb7716beb87da
cahetterich <hetterichcarla@gmail.com>
2024-01-17 17:42:12 +0100

    origamid js

---
+++ Iniciante/atribuicao-ternario.js
@@ -0,0 +1,41 @@
+/*
+Atribuição e Ternário
+*/
+
+/* COMENTÁRIOS
+
+Servem para explicar o código
+*/
+
+// Comentário de uma linha
+
+/*
+Comentário
+com diversas
+linhas
+*/
+
+// var nome = 'André';
+
+//Comentar uma linha de código desativa a mesma. Não deixe linhas de código comentadas no arquivo final.
+
+/*
+OPERADORES DE ATRIBUIÇÃO
+
+Podem funcionar como formas abreviadas
+*/
+
+var x = 5;
+var y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+/*
+OPERADOR TERNÁRIO
+
+Abreviação de condicionais com if e else
+*/
