704cf5a0852d24171845ad8fdb595f87d610c649
cahetterich <hetterichcarla@gmail.com>
2024-01-20 20:39:04 +0100

    origamid js

---
+++ 02 - Dom para Iniciantes/1dom.js
@@ -0,0 +1,18 @@
+/*
+DOCUMENT OBJECT MODEL (DOM)
+
+É uma interface que representa documentos HTML e XML através de objetos. Com ela é possível manipular a estrutura, estilo e conteúdo destes documentos.
+*/
+
+console.log(window);
+// window é o objeto global do browser
+// possui diferentes métodos e propriedades
+
+window.innerHeight; // retorna a altura do browser - metodo
+
+//Ao inspecionar elemento com o Chrome, você está vendo a representação oficial do DOM.
+
+/*
+DOM
+ */
+
